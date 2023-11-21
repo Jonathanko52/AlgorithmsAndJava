3041fc3d7c7d280276f31ab3da684f43c29c101a
Jonk <jonathan.ko523@gmail.com>
2023-11-20 18:56:55 -0800

    initial push

---
+++ src/Exercise1.java
@@ -0,0 +1,15 @@
+public class Exercise1 {
+
+
+    public static void main(String[] args) {
+        int myNum = (0+15)/2;
+        double myFloatNum = 2.0e-6 * 100000000.1;
+        boolean MyTruthy = true && false || true && true;
+
+
+        System.out.println("ANSWER ONE " + myNum);
+        System.out.println(myFloatNum);
+        System.out.println(MyTruthy);
+    }
+
+}
